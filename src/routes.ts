5336bfe33a37012984d30a0295194778f7dbc924
unknown <arango.f@proimpo.com>
2020-11-27 18:27:13 -0500

    configuracion inicial de hapi

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import {Server, Request, ResponseToolkit} from '@hapi/hapi'
+
+export const routes = (server: Server) => {
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request: Request, h: ResponseToolkit) => {
+      return 'Desde api Hapi';
+    }
+  });
+}
